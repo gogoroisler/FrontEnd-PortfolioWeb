b68cc2f70bc5d61b966a8f41c42a0502d2fced30
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2022-07-30 19:43:54 -0300

    presentacion

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -10,7 +10,7 @@ import { persona } from '../model/persona.model';
 
 /*desde donde toma el front la base de datos la informacion*/
 export class PersonaService {
-  URL='http://localhost:8080/personas/';
+  URL='https://backend-portfolio-web-sgr.herokuapp.com/personas/';
 
 /**constructor del servicio toma el metodo de netbeans */
   constructor(private http: HttpClient) { }
